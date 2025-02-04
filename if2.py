6f3ec52f96f45cec26e06b7a98ffef46afe42b65
becca <beccakamau101@gmail.com>
2025-02-04 12:38:08 +0300

    initial commit

---
+++ if2.py
@@ -0,0 +1,6 @@
+age=int(input("Enter number: "))
+if age  > 18 :
+        print(f"{age} vote!!")
+
+else:
+    print((f"{age} cannot vote"))
---
+++ if_else_statement.py
@@ -0,0 +1,15 @@
+score=float(input("Enter your Score"))
+if score>=90:
+        grade="A"
+
+elif score>=80:
+            grade="B"
+
+elif score>=70:
+            grade="C"
+elif score>=60:
+            grade="D"
+elif score >=50:
+            grade="E"
+
+print(f"Your grade is {grade}")
