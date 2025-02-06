5ee51133ec4ff5c4534f0ded7d09100bd613732b
becca <beccakamau101@gmail.com>
2025-02-06 12:38:52 +0300

    initial commit

---
+++ loops.py
@@ -0,0 +1,20 @@
+# for loop(sequence)
+fruits = ['banana' ,'mangoes' ,'orange', 'grape','apple' ]
+name="Becca"
+number=[1,2,3,4,5,6,7,8,9]
+for i in fruits:
+        print (i)
+for jina in name:
+        print(jina)
+    # while loop
+counter = 5
+while counter <=10:
+    print(counter)
+    counter +=1
+
+counter2=100
+while counter2>=0:
+    print (counter2)
+    counter2-=10
+for i in range (1,11):
+                print(i)
